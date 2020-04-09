dc6e7042a868e1ed3f1766b56a5e2ce53efb3f78
pesockiy <loremipsum.pt@gmail.com>
2020-04-09 15:12:15 +0300

    ///

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $(".select").select2();
+        console.warn("hjbuh");
+
+});
+
+
+//window.addEventListener('click', function(e) {
+//    e = event.target;
+//    if(!e.classList.contains('b-toBottom')) return;
+//    scrollBy(0, innerHeight);
+//});
