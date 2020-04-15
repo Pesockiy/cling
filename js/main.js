1d25193615d6c3056633489705f00e73d7f081f9
pesockiy <loremipsum.pt@gmail.com>
2020-04-15 12:21:58 +0300

    another commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
-    $(".select").select2();
+    $(".select").select2({
+        dropdownParent: $('.form__group--i')
+    });
+    
 
 });
 
 
-//window.addEventListener('click', function(e) {
-//    e = event.target;
-//    if(!e.classList.contains('b-toBottom')) return;
-//    scrollBy(0, innerHeight);
-//});
+window.addEventListener('click', function(e) {
+    e = event.target;
+    if(!e.classList.contains('b-toBottom')) return;
+    scrollBy(0, innerHeight);
+});
