be21640fc7b2568f7741e0d828912154845afb16
pesockiy <loremipsum.pt@gmail.com>
2020-05-10 21:46:42 +0300

    dfgdfg

--- js/main.js
+++ js/main.js
@@ -1,20 +1,18 @@
 $(document).ready(function () {
-    heighttoScroll = window.innerHeight
+    
     $(".scrollToNext").click(function () {
         let top = $(this).closest('section').next().offset().top;
-        $('html,body').animate({scrollTop: top}, 900);
+        $('html,body').animate({
+            scrollTop: top
+        }, 300);
     });
 
     $(".select2").each(function () {
         $(this).select2({
             minimumResultsForSearch: -1,
-            closeOnSelect: false,
-            selectOnClose: true,
             dropdownParent: $(this).closest('.form__group')
         })
-            
-});
-    $(".select2").on('select2:select', function (e) { 
-    console.log('select event');
-});
+
+    });
+
 });
