9891718f0060ec484937bca64ea7086f4e51ecdf
pesockiy <loremipsum.pt@gmail.com>
2020-05-18 23:52:22 +0300

    dfs

--- js/main.js
+++ js/main.js
@@ -1,18 +1,28 @@
 $(document).ready(function () {
-    
-    $(".scrollToNext").click(function () {
-        let top = $(this).closest('section').next().offset().top;
-        $('html,body').animate({
-            scrollTop: top
-        }, 300);
-    });
 
-    $(".select2").each(function () {
-        $(this).select2({
-            minimumResultsForSearch: -1,
-            dropdownParent: $(this).closest('.form__group')
-        })
+	$(".scrollToNext").click(function () {
+		let top = $(this).closest('section').next().offset().top;
+		$('html,body').animate({
+			scrollTop: top
+		}, 300);
+	});
+	let a = Array.from($('header > a'));
+	let u = Array.from($('section'));
+	window.onclick(function (e) {
+		e = event.target;
+		if(e.tagName != a) return;
+		let g = a.indexOf(e);
+		let w = u[g];
+		$('html,body').animate({
+			scrollTop: w;
+		}, 300);
+	});
+	$(".select2").each(function () {
+		$(this).select2({
+			minimumResultsForSearch: -1,
+			dropdownParent: $(this).closest('.form__group')
+		})
 
-    });
+	});
 
 });
